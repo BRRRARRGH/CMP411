0b8a4533b5b520feff87f5e92a0593abc53ac77f
BRRRARRGH <thure.w.johnson@gmail.com>
2025-09-19 11:47:38 -0500

    Finished Project for mod 3 exercize

--- Module 3/Javascript Sampling and Agile Practices/code.js
+++ Module 3/Javascript Sampling and Agile Practices/code.js
@@ -1,10 +1,48 @@
 let List1 = ""
 let List2 = ""
-const form = document.getElementById('firstForm');
-document.getElementById("List-1-Text").innerHTML = "Thank you for your submission!";
 
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-    document.getElementById("List-1-Text").innerHTML = "Thank you for your submission!";
+function validateAndAdd(){
+    if(document.getElementById("input").value == "")
+    {
+        alert("Please enter a word to add")
+    } 
+    else if(document.getElementById("whichList").value != "1" && document.getElementById("whichList").value != "2")
+    {
+        alert("Please enter a 1 or a 2 for the list");
+        document.getElementById("whichList").value = "";
+    }
+    else
+    {
+        if(document.getElementById("whichList").value == "1")
+        {
+            List1 += document.getElementById("input").value + '<br>';
+            document.getElementById("List-1-Text").innerHTML = List1;
+        }
+        if(document.getElementById("whichList").value == "2")
+        {
+            List2 += document.getElementById("input").value + '<br>';
+            document.getElementById("List-2-Text").innerHTML = List2;
+        }
+        document.getElementById("whichList").value = "";
+        document.getElementById("input").value = "";
 
-});
+
+
+
+    }
+}
+
+
+
+function clearList1()
+{
+    List1 = "";
+    document.getElementById("List-1-Text").innerHTML = List1;
+}
+
+function clearList2()
+{
+    List2 = ""
+    document.getElementById("List-2-Text").innerHTML = List2;
+
+}
